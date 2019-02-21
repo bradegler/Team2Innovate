ac341f706a0ac5e4adcf1bad186633c6bfd91b11
Linda Wu <hanning.wu@oracle.com>
2019-02-21 17:42:57 -0500

    Thired push

--- ingredientsServices/routing/database.js
+++ ingredientsServices/routing/database.js
@@ -50,56 +50,25 @@ router.get('/locations/:id', async(req, res, next) => {
 });
 
 
-
-/*
-router.put('/ingredients/:id', async(req, res, next) => {
-    let id = req.params.id;
-    let type = req.body.type;
-    let name = req.body.name;
-    let description = req.body.description;
-    let price = req.body.price;
-    let color = req.body.color;
-    let itemsize = req.body.size;
-    let rating = req.body.rating;
-    console.log(`Type : ${type}, name : ${name}, description: ${description}, price: ${price}, color: ${color}, size: ${itemsize}, rating: ${rating},  id: ${id} `);
-    let updateQuery = `UPDATE ROBOTICS.PRODUCTS SET TYPE = :type, NAME = :name, description = :description, price = :price, color = :color, item_size = :itemsize, rating = :rating WHERE ID = :id`
-    console.log(updateQuery);
-    let update = await dbService.simpleExecute(updateQuery,
-        [type, name, description, price, color, itemsize, rating, id]);
-
-    res.send(update);
+// Get all Designed pizza 
+router.get('/modelpizza/:id', async(req, res, next) => {
+    let query = `SELECT * FROM MODELPIZZA where pizza_id= \'PIZZA-01\'`;
+    let items = await dbService.simpleExecute(query);
+    res.send(items.rows);
 });
 
-router.post('/ingredients', async(req, res, next) => {
-    let type = req.body.type;
-    let name = req.body.name;
-    let description = req.body.description;
-    let price = req.body.price;
-    let color = req.body.color;
-    let itemsize = req.body.size;
-    let rating = req.body.rating;
-    let extension = req.body.extension;
-    let productimage = req.body.productImage;
-    let originalfilename = req.body.originalFileName;
-    let insertQuery = `INSERT INTO ROBOTICS.PRODUCTS (type, name, description, price, color, item_size, rating, product_image, original_file_name) VALUES(:type, :name, :description, :price, :color, :itemsize, :rating, :productimage, :originalfilename)`;
+router.post('/modelpizza', async(req, res, next) => {
+    let ingre_name = req.body.ingre_name;
+    let origin = req.body.origin;
+    let total_quantity = req.body.total_quantity;
+    let total_calories = req.body.total_calories;
+    let pizza_id = req.body.pizza_id;
+    
+    let insertQuery = `INSERT INTO MODELPIZZA (INGRE_NAME,ORIGIN,TOTAL_QUANTITY,TOTAL_CALORIES,PIZZA_ID) VALUES(:ingre_name, :origin, :total_quantity, :total_calories, :pizza_id)`;
     console.log(insertQuery);
     let insert = await dbService.simpleExecute(insertQuery,
-        [type, name, description, price, color, itemsize, rating, `${productimage}.${extension}`, originalfilename]);
-    let path = await storageService.findFile(productimage);
-    console.log(path);
-    let uploadResult = await storageService.uploadFile('TestBucket', productimage, path, extension);
-    console.log(JSON.stringify(uploadResult));
-    //res.send(insert);
-    res.send(uploadResult);
-    //res.send(`Path returned by storageService is: ${path} and uploadResult is: ${JSON.stringify(uploadResult)}`);
+        [ingre_name, origin, total_quantity, total_calories, pizza_id]);
+    res.send(insert);
 });
-
-router.delete('/items/:id', async(req, res, next) => {
-    let id = req.params.id;
-    let deleteQuery = 'DELETE FROM ROBOTICS.PRODUCTS WHERE id = :id';
-    console.log(deleteQuery);
-    let deleteObj = await dbService.simpleExecute(deleteQuery, [id]);
-    res.send(deleteObj);
-});*/
  
 module.exports = router;
